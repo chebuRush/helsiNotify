b64c6b668106b2cf8a8cfe20228f3d6bd872541f
Panichkin Victor <panichkinvictor@gmail.com>
2018-02-03 17:37:32 +0200

    master pre-commit

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const http = require("http");
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
 const app = express();
 const port = process.env.PORT || 8090;
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(__dirname +"/public"));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/public`));
 
-let server = http.createServer(app);
+const server = http.createServer(app);
 server.listen(process.env.PORT || port);
 
-require("./app")(app);
-
-
+require('./app')(app);
 
 module.exports = server;
