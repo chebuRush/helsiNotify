d442d7d7ddf7626875f90986c3589b9aaf408deb
Panichkin Victor <panichkinvictor@gmail.com>
2018-02-06 13:40:24 +0200

    finish with checkSeparateUser

--- Backend/notify/notifyUsersAndGetMoney.js
+++ Backend/notify/notifyUsersAndGetMoney.js
@@ -1,7 +1,37 @@
 const Firebase = require('./../FireBase');
 const notifyUser = require('./emailNotification');
+const config = require('config');
 
-function checkSeparateUser(uid, link, arrayOfDates) {
+const ONE_DOCTOR_VISIT_COST = config.get('ONE_DOCTOR_VISIT_COST');
+
+function sweepDBAndGetMoney(uid, link, keyForUsersDoctor, keyForDoctorList) {
+    return new Promise((resolve, reject) =>
+        Firebase.DataBase
+            .updateData(`users/${uid}`, {
+                doctors: {
+                    [keyForDoctorList]: {
+                        status: 2
+                    }
+                }
+            })
+            .then(() => Firebase.DataBase.deleteData('doctorList', link, keyForDoctorList))
+            .then(() =>
+                Firebase.DataBase.updateSensitiveData(uid, money => {
+                    if (money !== null) {
+                        return {
+                            available: +money.available,
+                            freezed: +money.freezed - ONE_DOCTOR_VISIT_COST,
+                            used: +money.used + +ONE_DOCTOR_VISIT_COST
+                        };
+                    }
+                    return null;
+                })
+            )
+            .catch(e => reject(e))
+    );
+}
+
+function checkSeparateUser(uid, keyForDoctorList, link, arrayOfDates) {
     return new Promise((resolve, reject) => {
         Firebase.DataBase
             .getData({}, `users/${uid}/doctors`, 'userDoctors')
@@ -20,14 +50,13 @@ function checkSeparateUser(uid, link, arrayOfDates) {
                     }
                 }
                 if (fittedDataIndex !== -1) {
-                    // TODO get uid emailToNotify, send email with a notification that doctor appointment is available
                     notifyUser(uid, link);
-                    // TODO update DB with delete doctor(use neededKey before), transfer money from freezed to used
-                    cleanUserAndGetMoney(uid, link);
                     arrayOfDates.splice(fittedDataIndex, 1);
+                    return sweepDBAndGetMoney(uid, link, neededKey, keyForDoctorList);
                 }
             })
-            .catch(e => console.error(e.message));
+            .then(() => resolve())
+            .catch(e => reject(e));
     });
 }
 
@@ -43,7 +72,8 @@ function getUsersForDoctorLink(link) {
 async function notifyUsersAndGetMoney(link, arrayOfDates) {
     await getUsersForDoctorLink();
 }
-checkSeparateUser('N8sesFHEckbEaL9x6UG43U6hc0O2', 'https://helsi.me/doctor/shv_20', [
+
+checkSeparateUser('N8sesFHEckbEaL9x6UG43U6hc0O2', '-L4XKcRNdgGoZIh593lM', 'https://helsi.me/doctor/shv_20', [
     '2018-02-05',
     '2018-02-06',
     '2018-02-07',
@@ -55,5 +85,6 @@ checkSeparateUser('N8sesFHEckbEaL9x6UG43U6hc0O2', 'https://helsi.me/doctor/shv_2
     '2018-02-15',
     '2018-02-16'
 ]);
+
 // getUsersForDoctorLink('https://helsi.me/doctor/shv_20');
 module.exports = notifyUsersAndGetMoney;
