1db0c8296db4f4f22f10cfc08f95034af20c8881
Panichkin Victor <panichkinvictor@gmail.com>
2018-02-16 19:05:54 +0200

    new build

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const busboyBodyParser = require('busboy-body-parser');
+// const bodyParser = require('body-parser');
+const busboy = require('express-busboy');
 
 const http = require('http');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(busboyBodyParser({ limit: '50mb' }));
 app.use(express.static(`${__dirname}/public`));
+busboy.extend(app);
 
 const server = http.createServer(app);
 server.listen(process.env.PORT || port);
