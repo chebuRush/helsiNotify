ce732a9443d6f93d8e2ec58fdbcf6b04d19990c3
Panichkin Victor <panichkinvictor@gmail.com>
2018-02-19 20:57:27 +0200

    memwatch added

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const notifyModule = require('./Backend/notify/');
 const busboy = require('express-busboy');
 const https = require('https');
 const http = require('http');
+const memwatch = require('memwatch-next');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));)
 busboy.extend(app);
 
 const server = http.createServer(app);
 server.listen(process.env.PORT || port);
 
 require('./Backend/queries')(app);
+
+memwatch.on('leak', info => {
+    console.error(`Memory leak detected | ${new Date()}:\n`, info);
+});
 
 try {
     notifyModule();
