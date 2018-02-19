8b8965627a4a46d29effb08fedf13c65f5e210ba
Panichkin Victor <panichkinvictor@gmail.com>
2018-02-19 22:10:47 +0200

    change http to https

--- Backend/notify/index.js
+++ Backend/notify/index.js
@@ -2,7 +2,6 @@ const getDoctorsListFromDB = require('./getDoctorsListFromDB');
 const visitDoctorPage = require('./visitDoctor').visitDoctor;
 const checkAvailability = require('./visitDoctor').checkAvailability;
 const deleteUnvalidLink = require('./deleteUnvalidLink');
-const memwatch = require('memwatch-next');
 const notifyUsersAndGetMoney = require('./notifyUsersAndGetMoney');
 const phantom = require('phantom');
 
@@ -10,10 +9,7 @@ async function WorkWithSeparateDoctor(doc, theArray) {
     for (const entry of theArray) {
         let schedule;
         try {
-            const hd = new memwatch.HeapDiff();
             schedule = await visitDoctorPage(doc, entry);
-            const diff = hd.end();
-            console.log('HeapDiff | visitDoctorPage: \n', diff);
         } catch (e) {
             if (e.message === 'Invalid doctor link') {
                 deleteUnvalidLink(entry);
@@ -34,19 +30,20 @@ function timeout(ms) {
 }
 
 async function main() {
-    const instance = await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']);
+    const instance = await phantom.create([
+        '--ignore-ssl-errors=yes',
+        '--load-images=no',
+        '--disk-cache=no',
+        '--max-disk-cache-size=1000'
+    ]);
     const doc = await instance.createPage();
     let docList = await getDoctorsListFromDB();
     Promise.resolve()
         .then(async function resolver() {
             if (docList) {
-                const hd = new memwatch.HeapDiff();
                 return WorkWithSeparateDoctor(doc, Object.keys(docList))
                     .then(async () => {
                         docList = await getDoctorsListFromDB();
-                        await doc.clearCookies();
-                        const diff = hd.end();
-                        console.log('HeapDiff | WorkWithSeparateDoctor: \n', diff);
                         await timeout(60000);
                     })
                     .then(resolver);
--- Backend/notify/visitDoctor.js
+++ Backend/notify/visitDoctor.js
@@ -1,19 +1,20 @@
-const cheerio = require('cheerio');
 
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function getDateInFormat(dt) {
-    return `${dt.getFullYear()}-${dt.getMonth() + 1 < 10 ? `0${dt.getMonth() + 1}` : dt.getMonth() + 1}-${dt.getDate() < 10 ? `0${dt.getDate()}` : dt.getDate()}`;
-}
 
 async function visitDoctor(doc, link) {
+
+    function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function getDateInFormat(dt) {
+        return `${dt.getFullYear()}-${dt.getMonth() + 1 < 10 ? `0${dt.getMonth() + 1}` : dt.getMonth() + 1}-${dt.getDate() < 10 ? `0${dt.getDate()}` : dt.getDate()}`;
+    }
+
     await doc.open(link);
     let dt = new Date();
     await timeout(1000);
     let html = await doc.property('content');
-    const $ = cheerio.load(html);
+    const $ = require('cheerio').load(html);
     if ($('.not-find-page__title').text() === '404') {
         throw new Error('Invalid doctor link');
     }
@@ -25,6 +26,8 @@ async function visitDoctor(doc, link) {
         schedulePlaces[getDateInFormat(dt)] = $(elem).find('.schedule__range-count').text();
         dt.setDate(dt.getDate() + 1);
     });
+    await doc.clearCookies();
+    
     return schedulePlaces;
 }
 
